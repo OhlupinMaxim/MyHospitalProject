78279d6a6f39e93da76058bb20ab6b0c8293e381
Максим Охлупин <maximohlupin.omd@yandex.ru>
2021-03-31 22:13:00 +0300

    add PAtient Recipe View

---
+++ src/main/java/views/PatientView.java
@@ -0,0 +1,131 @@
+package views;
+
+import DAO.PatientDAO;
+import com.vaadin.data.provider.ListDataProvider;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.ui.*;
+import model.Patient;
+import model.People;
+
+import java.sql.SQLException;
+
+//import static org.hospital.MyUI.EDITPATIENTVIEW;
+
+
+public class PatientView extends VerticalLayout implements View {
+    private Grid<Patient> TablePatient = null;
+    public PatientView() throws SQLException {
+
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+        VerticalLayout verticalLayout = new VerticalLayout();
+        addComponent(horizontalLayout);
+        //кнопка выхода на главную
+        Button return_button = new Button("На главную",
+                (Button.ClickListener) clickEvent -> getUI().getNavigator().navigateTo(""));
+        horizontalLayout.addComponent(verticalLayout);
+        verticalLayout.addComponent(return_button);
+        verticalLayout.setComponentAlignment(return_button, Alignment.TOP_RIGHT);
+
+        //таблица пациентов
+        Grid<Patient> grid = new Grid<>();
+        PatientDAO patientDAO = new PatientDAO() ;
+        grid.addColumn(People::getFirst_name).setCaption("Имя");
+        grid.addColumn(People::getSecond_name).setCaption("Отчество");
+        grid.addColumn(People::getThird_name).setCaption("Фамилия");
+        grid.addColumn(Patient::getPhone_number).setCaption("Номер Телефона");
+        try {
+            grid.setItems(patientDAO.getAll());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        TablePatient = grid;
+        horizontalLayout.addComponent(grid);
+        horizontalLayout.setComponentAlignment(grid, Alignment.BOTTOM_LEFT);
+
+        //добавить пациента
+        TextField f_name = new TextField("Имя","");
+        TextField s_name = new TextField("Отчество","");
+        TextField t_name = new TextField("Фамилия","");
+        TextField num = new TextField("Номер телефона","");
+
+        Button insert = new Button("Добавить", new Button.ClickListener(){
+            @Override
+            public void buttonClick(Button.ClickEvent clickEvent) {
+                PatientDAO patientDAO = null;
+                try {
+                    patientDAO = new PatientDAO();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+                try {
+                    patientDAO.insert(new Patient(
+                            f_name.getValue(),
+                            s_name.getValue(),
+                            t_name.getValue(),
+                            num.getValue()));
+                    ListDataProvider<Patient> dataProvider = (ListDataProvider<Patient>) grid
+                            .getDataProvider();
+                    dataProvider.getItems().add(new Patient(
+                            f_name.getValue(),
+                            s_name.getValue(),
+                            t_name.getValue(),
+                            num.getValue()));
+                    dataProvider.refreshAll();
+
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+                f_name.clear();
+                s_name.clear();
+                t_name.clear();
+                num.clear();
+            }
+        });
+        horizontalLayout.addComponents(verticalLayout);
+        verticalLayout.addComponents(f_name,s_name,t_name,num,insert);
+
+        //удалить пациента
+        Button delete = new Button("Удалить", new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent clickEvent) {
+                PatientDAO patientDAO = null;
+                try {
+                    patientDAO = new PatientDAO();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+                try {
+                    patientDAO.delete(patientDAO.getByFIO(grid.asSingleSelect().getValue().getFirst_name(),
+                            grid.asSingleSelect().getValue().getSecond_name(),
+                            grid.asSingleSelect().getValue().getThird_name()).getID());
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+                ListDataProvider<Patient> dataProvider = (ListDataProvider<Patient>) grid
+                        .getDataProvider();
+                dataProvider.getItems().remove(grid.asSingleSelect().getValue());
+                dataProvider.refreshAll();
+            }
+        });
+
+        //изменить запись
+
+        Button update = new Button("Изменить", (Button.ClickListener)
+                clickEvent -> {
+//                    getUI().getNavigator().navigateTo(EDITPATIENTVIEW);
+                    //UI.getCurrent().getPage().reload();
+                });
+        addComponents(update,delete);
+
+        addComponents(delete,update);
+    }
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent viewChangeEvent) {
+
+    }
+
+    public Grid<Patient> getTablePatient(){
+        return TablePatient;
+    }
+}
---
+++ src/main/java/views/RecipeView.java
@@ -0,0 +1,178 @@
+package views;
+
+import DAO.DoctorDAO;
+import DAO.PatientDAO;
+import DAO.RecipeDAO;
+import com.vaadin.data.provider.ListDataProvider;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.ui.*;
+import model.Doctor;
+import model.Patient;
+import model.People;
+import model.Recipe;
+
+import java.sql.SQLException;
+
+import static org.hospital.MyUI.*;
+
+public class RecipeView extends VerticalLayout implements View {
+    public RecipeView() throws SQLException, ClassNotFoundException {
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+        //кнопка выхода на главную
+        Button return_button = new Button("На главную",
+                (Button.ClickListener) clickEvent -> getUI().getNavigator().navigateTo(""));
+
+        //таблица врачей
+        //Grid<Doctor> grid1 = new DoctorView().getTableDoctor();
+        Grid<Doctor> grid1 = new Grid<>("Для добавления, выберете строчку");
+        DoctorDAO doctorDAO = new DoctorDAO();
+        grid1.addColumn(People::getFirst_name).setCaption("Имя");
+        grid1.addColumn(People::getSecond_name).setCaption("Отчество");
+        grid1.addColumn(People::getThird_name).setCaption("Фамилия");
+        grid1.addColumn(Doctor::getSpecialization).setCaption("Специализация");
+        try {
+            grid1.setItems(doctorDAO.getAll());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
+        //таблица пациентов
+        //Grid<Patient> grid2 = new PatientView().getTablePatient();
+        Grid<Patient> grid2 = new Grid<>("Для добавления, выберете строчку");
+        PatientDAO patientDAO = new PatientDAO();
+        grid2.addColumn(People::getFirst_name).setCaption("Имя");
+        grid2.addColumn(People::getSecond_name).setCaption("Отчество");
+        grid2.addColumn(People::getThird_name).setCaption("Фамилия");
+        grid2.addColumn(Patient::getPhone_number).setCaption("Номер Телефона");
+        try {
+            grid2.setItems(patientDAO.getAll());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
+        //таблица рецептов
+        Grid<Recipe> grid = new Grid<>();
+        RecipeDAO recipeDAO = new RecipeDAO();
+
+        grid.addColumn(Recipe::getDescription).setCaption("Описание");
+        grid.addColumn(recipe -> {
+            return recipe.getPatient().getFIO();
+        }).setCaption("Пациент");
+        grid.addColumn(recipe -> {
+            return recipe.getDoctor().getFIO();
+        }).setCaption("Доктор");
+        grid.addColumn(Recipe::getDate).setCaption("Дата назначения");
+        grid.addColumn(Recipe::getValidity).setCaption("Дата окончания");
+        grid.addColumn(Recipe::getPriority).setCaption("Приоритет");
+
+        try {
+            grid.setItems(recipeDAO.getAll());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
+        //добавить рецепт
+        TextArea descr = new TextArea("Описание", "");
+        TextField data = new TextField("Дата назначения YYYY-MM-DD", "");
+        TextField validity = new TextField("Дата окончания YYYY-MM-DD", "");
+        TextField priority = new TextField("Приоритет (Важный или Нормальный)", "");
+
+        Button insert = new Button("Добавить", new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent clickEvent) {
+                RecipeDAO recipeDAO = null;
+                try {
+                    recipeDAO = new RecipeDAO();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    recipeDAO.insert(new Recipe(
+                            descr.getValue(),
+                            grid2.asSingleSelect().getValue(),
+                            grid1.asSingleSelect().getValue(),
+                            data.getValue(),
+                            validity.getValue(),
+                            priority.getValue())
+                    );
+
+                    ListDataProvider<Recipe> dataProvider = (ListDataProvider<Recipe>) grid
+                            .getDataProvider();
+                    dataProvider.getItems().add(new Recipe(
+                            descr.getValue(),
+                            grid2.asSingleSelect().getValue(),
+                            grid1.asSingleSelect().getValue(),
+                            data.getValue(),
+                            validity.getValue(),
+                            priority.getValue()));
+                    dataProvider.refreshAll();
+                    descr.clear();
+                    data.clear();
+                    validity.clear();
+                    priority.clear();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+                UI.getCurrent().getPage().reload();
+            }
+        });
+
+        //удалить рецепт
+        Button delete = new Button("Удалить", new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent clickEvent) {
+                RecipeDAO recipeDAO = null;
+                try {
+                    recipeDAO = new RecipeDAO();
+                } catch (SQLException | ClassNotFoundException throwables) {
+                    throwables.printStackTrace();
+                }
+                try {
+                    recipeDAO.delete(grid.asSingleSelect().getValue().getID());
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+                ListDataProvider<Recipe> dataProvider = (ListDataProvider<Recipe>) grid
+                        .getDataProvider();
+                dataProvider.getItems().remove(grid.asSingleSelect().getValue());
+                dataProvider.refreshAll();
+            }
+        });
+
+        //изменить запись
+        Button update = new Button("Изменить", (Button.ClickListener)
+                clickEvent -> {
+//                    getUI().getNavigator().navigateTo(EDITRECIPEVIEW);
+                    UI.getCurrent().getPage().reload();
+                });
+        addComponents(update, delete);
+
+        Button filter = new Button("Фильтр", (Button.ClickListener)
+                clickEvent -> {
+//                    getUI().getNavigator().navigateTo(FILTERVIEW);
+                    //UI.getCurrent().getPage().reload();
+                });
+        addComponents(update, delete);
+
+        addComponents(return_button, filter);
+        setComponentAlignment(return_button, Alignment.TOP_LEFT);
+        addComponents(grid, horizontalLayout);
+        horizontalLayout.addComponents(descr, insert, delete, update);
+        addComponents(data, validity, priority);
+        addComponents(grid1, grid2);
+        //addComponents(delete, update);
+
+
+    }
+
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent viewChangeEvent) {
+
+    }
+}
